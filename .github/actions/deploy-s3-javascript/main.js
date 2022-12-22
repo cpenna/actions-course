095fadc7c5d81ee77032ba34ed090dd2208c57ee
Cesar Penna <cesar.penna@gmail.com>
2022-12-22 16:00:31 -0300

    Test javascript action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+
+run => {
+    //  Get input values
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const distFolder = core.getInput('dist-folder', { required: true })
+    const author = github.author
+
+    //  Upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+    core.notice(`Test javascript action from ${author}`)
+}
+
+run()
