1a64c72ff9c6cf98a593643e46aa2d486c38c3f6
Cesar Penna <cesar.penna@gmail.com>
2022-12-22 16:37:14 -0300

    function declaration updated

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const exec = require('@actions/exec')
 
-run => {
+function run () {
     //  Get input values
     const bucket = core.getInput('bucket', { required: true })
     const bucketRegion = core.getInput('bucket-region', { required: true })
